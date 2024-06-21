f6fbf15127a5a9ad971cf8ef411401f6550fdc8c
jdav892 <jadavila9@gmail.com>
2024-06-21 01:37:43 -0400

    started linkedin

---
+++ linkedin.js
@@ -0,0 +1,8 @@
+const removeAds = () => {
+    //Get all 'span' elements on the page
+    const spans = document.getElementsByTagName("span");
+
+    for(let i = 0; i < spans.length; i++){
+        
+    }
+}
