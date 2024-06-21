bc0e906a636a93098d1fdd9f66db5bf113c8f2a8
jdav892 <jadavila9@gmail.com>
2024-06-21 03:11:26 -0400

    finished

--- linkedin.js
+++ linkedin.js
@@ -3,6 +3,29 @@ const removeAds = () => {
     const spans = document.getElementsByTagName("span");
 
     for(let i = 0; i < spans.length; i++){
+        //check if they contain Suggested
+        if(spans[i].innerHTML === 'Suggested'){
+            //get the div that wraps the ad
+            let card = spans[i].closest(".feed-shared-update-v2");
+            //if class changed get parent
+            if (card === null) {
+            //could also be card
+                const j = 0;
+                card = spans[i];
+                while(j < 6){
+                    card = card.parentNode;
+                    j++;
+                }
+            }
+            //change css display to none to hide ad
+            card.setAttribute("style", "display: none !important;");
+        }
         
     }
 }
+
+removeAds();
+
+setInterval(() =>{
+    removeAds;
+}, 100)
